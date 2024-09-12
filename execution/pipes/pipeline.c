9d8c7eaf4ab17726e117c0ced343e5cc042885a9
bzinedda <bzinedda@student.1337.ma>
2024-09-12 19:48:45 +0100

    added input redirection between pipes

--- execution/pipes/pipeline.c
+++ execution/pipes/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: bzinedda <bzinedda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 17:25:49 by bzinedda          #+#    #+#             */
-/*   Updated: 2024/09/12 17:04:57 by bzinedda         ###   ########.fr       */
+/*   Updated: 2024/09/12 19:43:10 by bzinedda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -202,7 +202,7 @@
 #include "../../minishell.h"
 #include <sys/wait.h>
 #include <errno.h>
-#define MAX_FDS 1024  // Adjust this based on your system's limits
+#define MAX_FDS 1024 // Adjust this based on your system's limits
 
 const char *get_path(const char *cmd, t_env *env)
 {
@@ -237,151 +237,178 @@ const char *get_path(const char *cmd, t_env *env)
 	return (NULL);
 }
 
-
 void redirections(t_opp *operator)
 {
-    t_opp *op;
+	t_opp *op;
 
-    op = operator;
-    if (!operator)
-        return;
-    while (op)
-    {
-        if (op->operator== RED_OUT)
-        {
-            int fd_out = open(op->arg, O_CREAT | O_RDWR | O_TRUNC, 0644);
-            dup2(fd_out, STDOUT_FILENO);
-            close(fd_out);
-        }
-        if (op->operator== APPEND)
-        {
-            int fd_out = open(op->arg, O_CREAT | O_RDWR | O_APPEND, 0644);
-            dup2(fd_out, STDOUT_FILENO);
-            close(fd_out);
-        }
-        op = op->next;
-    }
+	op = operator;
+	if (!operator)
+		return;
+	while (op)
+	{
+		if (op->operator== RED_OUT)
+		{
+			int fd_out = open(op->arg, O_CREAT | O_RDWR | O_TRUNC, 0644);
+			dup2(fd_out, STDOUT_FILENO);
+			close(fd_out);
+		}
+		if (op->operator== APPEND)
+		{
+			int fd_out = open(op->arg, O_CREAT | O_RDWR | O_APPEND, 0644);
+			dup2(fd_out, STDOUT_FILENO);
+			close(fd_out);
+		}
+		op = op->next;
+	}
 }
 
 void execute_command(t_data *data, char **commands)
 {
-    const char *full_path;
+	const char *full_path;
 
-    full_path = get_path(commands[0], data->env);
-    if (full_path != NULL)
-    {
-        execve(full_path, commands, env_to_array(data->env));
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        fprintf(stderr, "Command not found: %s\n", commands[0]);
-        exit(EXIT_FAILURE);
-    }
+	full_path = get_path(commands[0], data->env);
+	if (full_path != NULL)
+	{
+		execve(full_path, commands, env_to_array(data->env));
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		fprintf(stderr, "Command not found: %s\n", commands[0]);
+		exit(EXIT_FAILURE);
+	}
 }
 
 void redirect_files(t_com *command, int *fd, int curr_cmd, int num_commands)
 {
-    if (curr_cmd == 0)
+    t_opp *curr_op = command->operator;
+	int flags, fd_redir = -1;
+    // Redirect input from previous command if not the first command
+    if (curr_cmd > 0 && fd[0] != -1)
+        dup2(fd[0], STDIN_FILENO);
+
+    // Redirect output to next command if not the last command
+    if (curr_cmd < num_commands - 1 && fd[1] != -1)
+	{
+		while (curr_op)
+		{
+			if (command->operator->operator == RED_OUT)
+				dup2(fd_redir, STDOUT_FILENO);
+			curr_op = curr_op->next;
+		}
+	}
+	else
+        dup2(fd[1], STDOUT_FILENO);
+
+	curr_op = command->operator;
+    // Handle redirection operators
+    while (curr_op)
     {
-        // First command: only redirect stdout
-        if (!command->operator)
-              dup2(fd[1], STDOUT_FILENO);
-        else
-            redirections(command->operator);
-    }
-    else if (curr_cmd == num_commands - 1)
-    {
-        // Last command: only redirect stdin
-        if (!command->operator)
-               dup2(fd[0], STDIN_FILENO);
-        else
-            redirections(command->operator);
-    }
-    else
-    {
-        // Middle commands: redirect both stdin and stdout
-        if (!command->operator)
+        if (curr_op->operator)
         {
-            dup2(fd[0], STDIN_FILENO);
-            dup2(fd[1], STDOUT_FILENO);
+            // int flags, fd_redir = -1;
+            if (curr_op->operator)
+            {
+                if (RED_IN == curr_op->operator)
+				{
+                    fd_redir = open(curr_op->arg, O_RDONLY);
+                    dup2(fd_redir, STDIN_FILENO);
+                    break;
+				}
+                if (RED_OUT == curr_op->operator)
+				{
+                	flags = O_WRONLY | O_CREAT | O_TRUNC;
+                    fd_redir = open(curr_op->arg, flags, 0644);
+                    dup2(fd_redir, STDOUT_FILENO);
+               //   break;
+				}
+                // if (RED_OUT == curr_op->operator)
+				// {
+                // 	flags = O_WRONLY | O_CREAT | O_TRUNC;
+                //     fd_redir = open(curr_op->arg, flags, 0644);
+                //     dup2(fd_redir, STDOUT_FILENO);
+                //     break;
+				// }
+				
+            }
+            if (fd_redir != -1)
+                close(fd_redir);
         }
-        else
-            redirections(command->operator);
+        curr_op = curr_op->next;
     }
 }
 
 void close_all_fds(int *fds, int count)
 {
-    for (int i = 0; i < count; i++)
-    {
-        if (fds[i] != -1)
-        {
-            close(fds[i]);
-            fds[i] = -1;
-        }
-    }
+	for (int i = 0; i < count; i++)
+	{
+		if (fds[i] != -1)
+		{
+			close(fds[i]);
+			fds[i] = -1;
+		}
+	}
 }
 
 int ft_execute_pipeline(char ***commands, int num_commands, int *return_value, t_data *data)
 {
-    int fds[MAX_FDS];
-    int fd_count = 0;
-    pid_t pid;
-    int status;
+	int fds[MAX_FDS];
+	int fd_count = 0;
+	pid_t pid;
+	int status;
 
-    for (int i = 0; i < num_commands; i++)
-    {
-        int fd[2];
-        if (i < num_commands - 1)
-        {
-            if (pipe(fd) == -1)
-            {
-                perror("pipe");
-                close_all_fds(fds, fd_count);
-                return 1;
-            }
-            fds[fd_count++] = fd[0];
-            fds[fd_count++] = fd[1];
-        }
+	for (int i = 0; i < num_commands; i++)
+	{
+		int fd[2];
+		if (i < num_commands - 1)
+		{
+			if (pipe(fd) == -1)
+			{
+				perror("pipe");
+				close_all_fds(fds, fd_count);
+				return 1;
+			}
+			fds[fd_count++] = fd[0];
+			fds[fd_count++] = fd[1];
+		}
 
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("fork");
-            close_all_fds(fds, fd_count);
-            return 1;
-        }
-        else if (pid == 0)
-        {
-            // Child process
-            redirect_files(data->command, fd, i, num_commands);
-            close_all_fds(fds, fd_count);  // Close all unused fds in child
-            execute_command(data, commands[i]);
-            exit(EXIT_FAILURE);  // In case execute_command fails
-        }
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			close_all_fds(fds, fd_count);
+			return 1;
+		}
+		else if (pid == 0)
+		{
+			// Child process
+			redirect_files(data->command, fd, i, num_commands);
+			close_all_fds(fds, fd_count); // Close all unused fds in child
+			execute_command(data, commands[i]);
+			exit(EXIT_FAILURE); // In case execute_command fails
+		}
 
-        // Parent process
-        if (i > 0)
-        {
-            close(fds[fd_count - 4]);  // Close read end of previous pipe
-            close(fds[fd_count - 3]);  // Close write end of previous pipe
-        }
+		// Parent process
+		if (i > 0)
+		{
+			close(fds[fd_count - 4]); // Close read end of previous pipe
+			close(fds[fd_count - 3]); // Close write end of previous pipe
+		}
 
-        data->command = data->command->next;
-    }
+		data->command = data->command->next;
+	}
 
-    close_all_fds(fds, fd_count);
+	close_all_fds(fds, fd_count);
 
-    // Wait for all child processes
-    while (wait(&status) > 0)
-    {
-        if (WIFEXITED(status))
-            *return_value = WEXITSTATUS(status);
-        else if (WIFSIGNALED(status))
-            *return_value = 128 + WTERMSIG(status);
-    }
+	// Wait for all child processes
+	while (wait(&status) > 0)
+	{
+		if (WIFEXITED(status))
+			*return_value = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			*return_value = 128 + WTERMSIG(status);
+	}
 
-    return 0;
+	return 0;
 }
