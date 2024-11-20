77f34f6a23ae1877ef2eb3d684e54c568f0091d9
Ahmed-Galadi <galadiahmed99@gmail.com>
2024-11-20 03:04:26 +0100

    UPDATES: - fiexed deleted dir 1/ after mkdir -p 1/2/3/4/

--- execution/builtings/ft_pwd.c
+++ execution/builtings/ft_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bzinedda <bzinedda@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 05:23:12 by bzinedda          #+#    #+#             */
-/*   Updated: 2024/10/31 15:41:19 by bzinedda         ###   ########.fr       */
+/*   Updated: 2024/11/20 02:59:48 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ int	ft_pwd(t_shell *data)
 	if (getcwd(buffer, sizeof(buffer)))
 		printf("%s\n", buffer);
 	else
+	{
+		if (data->pwd)
+			return (printf("%s\n", data->pwd), 0);
 		return (perror("getcwd"), 1);
+	}
 	return (0);
 }
